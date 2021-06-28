51748073822ca572db91f801d2d9691f657fb55c
Maia Stulic Fontan <maiastulicfontan@gmail.com>
2021-06-28 18:22:08 -0300

    feat: add project structure

---
+++ src/main/java/edu/ingsw/mstulicfontan/tpCICD/App.java
@@ -0,0 +1,11 @@
+package edu.ingsw.mstulicfontan.tpCICD;
+
+public class App {
+
+    public App() {}
+
+    protected int sumar (int a, int b) {
+        return a + b;
+    }
+
+}
---
+++ src/test/java/edu/ingsw/mstulicfontan/tpCICD/AppTest.java
@@ -0,0 +1,13 @@
+package edu.ingsw.mstulicfontan.tpCICD;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class AppTest {
+
+
+    @Test
+    protected void sumarTest() {
+        App app = new App();
+        Assertions.assertEquals(7, app.sumar(2,5));
+    }
+}
