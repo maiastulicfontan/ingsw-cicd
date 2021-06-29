78d481c8827ecdd11031fd09d77f3da6ec4a1c1c
Maia Stulic Fontan <maiastulicfontan@gmail.com>
2021-06-29 14:44:48 -0300

    feat: add dividirTest

--- src/test/java/edu/ingsw/mstulicfontan/tpCICD/AppTest.java
+++ src/test/java/edu/ingsw/mstulicfontan/tpCICD/AppTest.java
@@ -10,4 +10,10 @@ public class AppTest {
         App app = new App();
         Assertions.assertEquals(7, app.sumar(2,5), "Resultado erróneo");
     }
+
+    @Test
+    protected void dividirTest() {
+        App app = new App();
+        Assertions.assertEquals(4, app.dividir(8,2), "Resultado erróneo");
+    }
 }
