e5d5fc770207695cc07f1c5065044c799a5b6414
Maia Stulic Fontan <maiastulicfontan@gmail.com>
2021-06-28 22:30:02 -0300

    feat: add metodo "dividir"

--- src/test/java/edu/ingsw/mstulicfontan/tpCICD/AppTest.java
+++ src/test/java/edu/ingsw/mstulicfontan/tpCICD/AppTest.java
@@ -8,6 +8,6 @@ public class AppTest {
     @Test
     protected void sumarTest() {
         App app = new App();
-        Assertions.assertEquals(7, app.sumar(2,5));
+        Assertions.assertEquals(7, app.sumar(2,5), "Resultado erróneo");
     }
 }
