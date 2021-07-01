14d3660e3563386fedb6dd957cd90c997fb8b935
Maia Stulic Fontan <maiastulicfontan@gmail.com>
2021-07-01 11:30:43 -0300

    feat: add test coverage

--- src/main/java/edu/ingsw/mstulicfontan/tpCICD/App.java
+++ src/main/java/edu/ingsw/mstulicfontan/tpCICD/App.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Random;
 
 @RestController
 @EnableAutoConfiguration
 public class App {
+    Random random = new Random();
 
     public App() {}
 
@@ -21,7 +23,11 @@ public class App {
 
     @RequestMapping("/")
     String index() {
-        return "4 + 3 = " + this.sumar(4, 3);
+        String display;
+        int a = random.nextInt(100);
+        int b = random.nextInt(100);
+        display = "Suma random: "+ a + " + " + b + " = " + this.sumar(a,b);
+        return display;
     }
 
     public static void main(String[] args) {
--- src/test/java/edu/ingsw/mstulicfontan/tpCICD/AppTest.java
+++ src/test/java/edu/ingsw/mstulicfontan/tpCICD/AppTest.java
@@ -1,19 +1,25 @@
 package edu.ingsw.mstulicfontan.tpCICD;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class AppTest {
+    public App app;
 
+    @BeforeEach
+    void setUp() {
+        this.app = new App();
+    }
 
     @Test
     protected void sumarTest() {
-        App app = new App();
-        Assertions.assertEquals(7, app.sumar(2,5), "Resultado erróneo");
+        Assertions.assertEquals(7, app.sumar(2,5), "Resultado erroneo");
+        Assertions.assertEquals(4, app.sumar(4,0), "Resultado erroneo");
+        Assertions.assertEquals(2, app.sumar(-1, 3), "Resultado erroneo");
     }
 
     @Test
     protected void dividirTest() {
-        App app = new App();
-        Assertions.assertEquals(4, app.dividir(8,2), "Resultado erróneo");
+        Assertions.assertEquals(4, app.dividir(8,2), "Resultado erroneo");
     }
 }
